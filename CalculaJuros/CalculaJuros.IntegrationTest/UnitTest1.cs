c5ea1999f864ecb64964305cbc30b018ed39b98d
Matheus Andrade <matheus61@hotmail.com>
2020-05-20 18:55:30 -0300

    Adicionar arquivos de projeto.

---
+++ CalculaJuros/CalculaJuros.IntegrationTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace CalculaJuros.IntegrationTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ TaxaJuros/TaxaJuros.FunctionalTest/UnitTest1.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace TaxaJuros.FunctionalTest
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
