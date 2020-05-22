7d676b8b4a1c57e0d3d117829d481566005eb120
Matheus Andrade <matheus61@hotmail.com>
2020-05-22 01:04:51 -0300

    Testes Unitarios

---
+++ CalculaJuros.UnitTest/Controllers/CalculaJurosControllerUnitTest.cs
@@ -0,0 +1,39 @@
+﻿using CalculaJuros.Api.Controllers;
+using CalculaJuros.Api.Infrastructure;
+using Moq;
+using Xunit;
+
+namespace CalculaJuros.UnitTest.Controllers
+{
+    public class CalculaJurosControllerUnitTest
+    {
+        private Mock<ICalculaJurosService> _mockCalculaJurosService;
+        private CalculaJurosController _controller;
+
+        public CalculaJurosControllerUnitTest()
+        {
+            _mockCalculaJurosService = new Mock<ICalculaJurosService>();
+            _controller = new CalculaJurosController(_mockCalculaJurosService.Object);
+        }
+
+        public class CalculaJurosShould: CalculaJurosControllerUnitTest
+        {
+            [Trait("Category", "Unit")]
+            [Fact(DisplayName = "Api - CalculaJurosController - Should call calculaJuros service properly")]
+            public async void GetCalculaJuros()
+            {
+                decimal valorInicial = 1;
+                int tempo = 2;
+
+                _mockCalculaJurosService.Setup(mock => mock.calculaJuros(It.IsAny<decimal>(), It.IsAny<int>()));
+
+                var response = await _controller.Get(valorInicial, tempo);
+
+                _mockCalculaJurosService.Verify(
+                    mock => mock.calculaJuros(It.Is<decimal>(req => req == valorInicial), It.Is<int>(req => req == tempo)), 
+                    Times.Once, 
+                    "Calcula juros should be called properly");
+            }
+        }
+    }
+}
---
+++ TaxaJuros.UnitTest/Controllers/TaxaJurosControllerUnitTest.cs
@@ -0,0 +1,35 @@
+﻿using Moq;
+using TaxaJuros.Api.Controllers;
+using TaxaJuros.Api.Infrastructure;
+using Xunit;
+
+namespace TaxaJuros.UnitTest.Controllers
+{
+    public class TaxaJurosControllerUnitTest
+    {
+        private Mock<IJurosService> _mockJurosService;
+        private TaxaJurosController _controller;
+
+        public TaxaJurosControllerUnitTest()
+        {
+            _mockJurosService = new Mock<IJurosService>();
+            _controller = new TaxaJurosController(_mockJurosService.Object);
+        }
+
+        public class TaxaJurosShould : TaxaJurosControllerUnitTest
+        {
+            [Trait("Category", "Unit")]
+            [Fact(DisplayName = "Api - TaxaJurosController - Should call taxaJuros service properly")]
+            public void GetCalculaJuros()
+            {
+                _mockJurosService.Setup(mock => mock.getJuros());
+
+                var response = _controller.Get();
+
+                _mockJurosService.Verify(mock => mock.getJuros(),
+                    Times.Once,
+                    "Taxa juros should be called properly");
+            }
+        }
+    }
+}
